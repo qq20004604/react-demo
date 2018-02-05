cd138d2ffaec48ca955e2130af6bbdb09a51659a
qq20004604 <20004604@qq.com>
2018-02-05 21:04:51 +0800

    优化表格，将表格调整为高阶组件

--- 房贷计算器/src/components/table.js
+++ 房贷计算器/src/components/table.js
@@ -6,57 +6,103 @@ import style from '../css/main.css';
  * QQ: 20004604
  * weChat: qq20004604
  */
-export default class Table extends React.Component {
-    render() {
-        if (!this.props.data.length > 0) {
-            return <div></div>
+// 高阶组件写法
+function createTable (args) {
+    return class Table extends React.Component {
+        render () {
+            if (!this.props.data.length > 0) {
+                return <div></div>
+            }
+            return (
+                <table id={args.tableId} className={args.tableClass}>
+                    <thead style={args.theadClass}>
+                    <tr>
+                        {args.tds.map((td, i) => {
+                            if (typeof td.headContent === 'function') {
+                                return td.headContent(i)
+                            } else if (typeof td.headContent === 'string') {
+                                return <td key={i}>{td.headContent}</td>
+                            } else {
+                                return td.headContent
+                            }
+                        })}
+                    </tr>
+                    </thead>
+                    <tbody id={args.tbodyId} className={args.tbodyClass}>
+                    {this.props.data.map((data, index) => {
+                        return <tr key={index}>
+                            {args.tds.map((td, i) => {
+                                if (typeof td.bodyContent === 'function') {
+                                    return td.bodyContent(data, i)
+                                } else if (typeof td.bodyContent === 'string') {
+                                    return <td key={i}>{td.bodyContent}</td>
+                                } else {
+                                    return td.bodyContent
+                                }
+                            })}
+                        </tr>
+                    })}
+                    </tbody>
+                </table>)
         }
-        return (
-
-            <table id={style.table}>
-                <thead>
-                <tr>
-                    <td>年月</td>
-                    <td>本月还</td>
-                    <td>还款组成（本金 + 利息）</td>
-                    <td>剩余待还本金</td>
-                    <td>累计还了</td>
-                    <td>当前CPI通胀系数（相较第一年）</td>
-                    <td>计算CPI后当月还款价值</td>
-                    <td>计算CPI后已累计还款数</td>
-                </tr>
-                </thead>
-                <tbody id="tbody">
-                {this.props.data.map(data => {
-                    return (
-                        <tr key={`${data.year}${data.month}`}>
-                            <td>{data.year}年{data.month}月</td>
-                            {/*年 月*/}
-
-                            <td>{data.payThisMonth}</td>
-                            {/*本月还*/}
-
-                            <td style={{textAlign: 'left'}}>本金：{data.repaymentPerMonth} +
-                                利息：{data.interest}</td>
-                            {/*月还本金 + 利息*/}
-
-                            <td>{data.totalWithoutDownPaymentLeft}</td>
-                            {/*待还本金*/}
-
-                            <td>{data.totalRepayment}</td>
-                            {/*累计还*/}
-
-                            <td>{data.CPI}</td>
-                            {/*CPI*/}
-
-                            <td>{data.payThisMonthByCPI}</td>
-                            {/*本月还（算CPI）*/}
-
-                            <td>{data.totalByCPI}</td>
-                            {/*累计还（算CPI）*/}
-                        </tr>)
-                })}
-                </tbody>
-            </table>)
     }
 }
+
+let tableInfo = {
+    tds: [
+        {
+            headContent: function () {
+                return <td key='year'>年月</td>
+            },
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.year}年{data.month}月</td>
+            }
+        },
+        {
+            headContent: '本月还',
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.payThisMonth}</td>
+            }
+        },
+        {
+            headContent: <td key={'还款'}>还款组成<br/>[本金 + 利息]</td>,
+            bodyContent: function (data, index) {
+                return <td key={index} style={{textAlign: 'left'}}>本金：{data.repaymentPerMonth}<br/>
+                    利息：{data.interest}</td>
+            }
+        },
+        {
+            headContent: '剩余待还本金',
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.totalWithoutDownPaymentLeft}</td>
+            }
+        },
+        {
+            headContent: '累计还了',
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.totalRepayment}</td>
+            }
+        },
+        {
+            headContent: <td key='cpi'>当前CPI通胀系数<br/>（相较第一年）</td>,
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.CPI}</td>
+            }
+        },
+        {
+            headContent: '计算CPI后当月还款价值',
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.payThisMonthByCPI}</td>
+            }
+        },
+        {
+            headContent: '计算CPI后已累计还款数',
+            bodyContent: function (data, index) {
+                return <td key={index}>{data.totalByCPI}</td>
+            }
+        }
+    ],
+    tableId: style.table,
+    tbodyId: 'tbody'
+}
+export default createTable(tableInfo)
